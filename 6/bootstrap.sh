17c509bf979fcb4a00e59c2c246e4a446addde8f
b_sergey_u@mail.ru <89423715+baburinsy@users.noreply.github.com>
2021-12-16 03:25:29 +0300

    bootstrap.sh

---
+++ 6/bootstrap.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+
+#Установка python3 и python3-pip пакетов
+sudo apt install -y python3-venv 
+sudo apt install -y python3-pip
+
+#Установка Python библиотеки psycopg2
+python3 -m venv venv
+source venv/bin/activate
+sudo apt install libpq-dev
+pip install psycopg2
+
+#Установка django
+pip install django
+
+#Копирование пустого файла
+cp /sync-folder/null-file.txt /vagrant/null-file-copy.txt
